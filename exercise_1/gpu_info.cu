24751bf11ab96c4ff3fd9e9fde81fccd5206bc89
Falk358 <maxdanelli@gmail.com>
2023-10-20 15:56:26 +0200

    finished exercise 1

--- exercise_1/gpu_info.cu
+++ exercise_1/gpu_info.cu
@@ -7,9 +7,16 @@ int main()
 	{
 		cudaSetDevice(i);
 		cudaDeviceProp deviceProp;
+		int driverVersion;
+		int runtimeVersion;
+		cudaError_t error_id_driver = cudaDriverGetVersion(&driverVersion);
+		cudaError_t error_id_runtime = cudaRuntimeGetVersion(&runtimeVersion);
 		cudaGetDeviceProperties(&deviceProp, i);
-		printf("\nDevice %d: %s", i, deviceProp.name);	
+		printf("\nDevice %d: %s\n", i, deviceProp.name);
+		printf("	Driver Version %d\n 	Runtime Version: %d\n 	GPU Memory in MB: %d\n 	Max Num Threads per block: %d\n 	UVA available: %d", driverVersion, runtimeVersion, deviceProp.totalGlobalMem/1024/1024, deviceProp.maxThreadsPerBlock, deviceProp.unifiedAddressing);
+		
 	}
 
 
 }
+
