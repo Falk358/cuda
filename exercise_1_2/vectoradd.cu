058635f5c4af04bc5ab69f0a9171155a9f037ee8
Falk358 <maxdanelli@gmail.com>
2023-10-24 10:11:08 +0200

    finished exercise 1_2 and renamed folders correctly

--- exercise_1_2/vectoradd.cu
+++ exercise_1_2/vectoradd.cu
@@ -1,10 +1,12 @@
-__global__ void vec_add(int* x, int*y, int* res)
+#include <stdio.h>
+
+
+
+__global__ void vec_add(double* x, double*y, double* res)
 {
     int i = threadIdx.x;
     res[i] = x[i] + y[i];
 }
-
-
 
 
 int main()
@@ -17,18 +19,23 @@ int main()
     int vector_length = 1024;
 
     // memory allocation
-    size_t bytes = vector_length*sizeof(int);
-    int* h_mem_x = (int*) malloc(bytes);
-    int* h_mem_y = (int*) malloc(bytes);
-    int* h_mem_res = (int*) malloc(bytes);
-    int* d_mem_x;
-    int* d_mem_y;
-    int* d_mem_res;
+    size_t bytes = vector_length*sizeof(double);
+    double* h_mem_x = (double*) malloc(bytes);
+    double* h_mem_y = (double*) malloc(bytes);
+    double* h_mem_res = (double*) malloc(bytes);
+    double* d_mem_x;
+    double* d_mem_y;
+    double* d_mem_res;
     cudaMalloc(&d_mem_x, bytes);
     cudaMalloc(&d_mem_y, bytes);
     cudaMalloc(&d_mem_res, bytes);
     
     // TODO fill vectors h_mem_x and h_mem_y with data
+    for (int i = 0; i < vector_length; i++)
+    {
+        h_mem_x[i] = 1.0;
+        h_mem_y[i] = 2.0;
+    }
 
     //copy vectors to device
     cudaMemcpy(d_mem_x, h_mem_x, bytes, cudaMemcpyHostToDevice);
@@ -41,7 +48,16 @@ int main()
     cudaMemcpy(h_mem_res, d_mem_res, bytes, cudaMemcpyDeviceToHost);
 
     // TODO verify and print result
-
+    double tolerance = 1.0e-14;
+    bool error_compute = false;
+    for (int i = 0; i < vector_length; i++)
+    {
+        if (h_mem_res[i] - 3.0 > tolerance)
+        {
+            printf("error computing vector addition; Error exceeds %f at index %d of solution\n", tolerance, i);
+            error_compute = true;
+        }
+    }
     // free memory
     free(h_mem_x);
     free(h_mem_y);
@@ -50,5 +66,15 @@ int main()
     cudaFree(d_mem_x);
     cudaFree(d_mem_y);
     cudaFree(d_mem_res);
+
+    if (error_compute)
+    {
+        exit(1);
+    }
+    else
+    {
+        printf("Success! No errors during computation!\n");
+        exit(0);
+    }
     
 }
