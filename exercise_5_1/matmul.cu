0721e2dbf40e8c7825d19856295c33d0539f9c5e
Falk358 <maxdanelli@gmail.com>
2023-11-18 18:38:46 +0100

    fixed many bugs; todo measure performance for different sizes

--- exercise_5_1/matmul.cu
+++ exercise_5_1/matmul.cu
@@ -1,7 +1,8 @@
 #include <string>
+#include <math.h>
 #include <iostream>
 #include <cublas_v2.h>
-using namespace std;
+//using namespace std;
 
 
 // init n*n matrix filled with value val
@@ -32,9 +33,21 @@ double* initVector(long n, double val, size_t memsize)
 
 
 
-int verifyResult(double* result, long n_result, double tolerance)
+bool verifyResult(double* result, long n_result, double tolerance)
 {
-    return;
+    bool success = true;
+    double target = 20.0;
+    for (int i = 0; i < n_result; i++)
+    {
+        double difference = result[i] - target;
+        if (fabs(difference) > tolerance)
+        {
+            std::cout << "verification of matrix result at coordinates [" << i  << "] failed; difference is " << abs(difference) << std::endl;
+            std::cout << "value at coordinates [" << i <<  "] is:" << result[i] << std::endl;
+            success = false;
+        }
+    }
+    return success;
 }
 
 
@@ -43,16 +56,24 @@ int main(int argc, char* argv[])
 {
     if (argc < 2)
     {
-        cerr << "Error, not enough cmd line arguments given; usage matmul \"problem_size\", where problem size is an int";
+        std::cerr << "Error, not enough cmd line arguments given; usage matmul \"problem_size\", where problem size is an int";
         exit(1);
     }
-    int problem_size = stoi(argv[1]);
+
+    cublasHandle_t handle;
+    cublasStatus_t status = cublasCreate(&handle);
+    if (status != CUBLAS_STATUS_SUCCESS)
+    {
+        std::cout << "ERROR: cublasCreate failed. Error code: " << status << std::endl;
+        exit(1);
+    }
+
+    int problem_size = std::stoi(argv[1]);
     size_t vec_size = sizeof(double) *problem_size;
     size_t mat_size = sizeof(double) * problem_size* problem_size;
     double* vector = initVector(problem_size, 1.0, vec_size);
     double* matrix = initMatrix(problem_size, 2.0, mat_size);
-    double* result = (double*) malloc(mat_size);
-    
+    double* result = (double*) malloc(vec_size);
     double alpha = 1.0;
     double beta = 0.0;
     double* device_vector;
@@ -60,36 +81,34 @@ int main(int argc, char* argv[])
     double* device_result;
     cudaMalloc(&device_vector, vec_size);
     cudaMalloc(&device_matrix, mat_size);
-    cudaMalloc(&device_result, mat_size);
+    cudaMalloc(&device_result, vec_size);
 
-    cublasHandle_t handle;
-    cublasStatus_t status = cublasCreate(&handle);
-    if (status != CUBLAS_STATUS_SUCCESS)
-    {
-        cout << "ERROR: cublasCreate failed. Error code: " << status << endl;
-        exit(1);
-    }
+    cudaMemcpy(device_vector, vector, vec_size, cudaMemcpyHostToDevice);
+    cudaMemcpy(device_matrix, matrix, mat_size, cudaMemcpyHostToDevice);
 
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
-    
     cudaEventRecord(start,0);
 
-    //TODO computation here
-    cublasStatus_t status_computation = cublasDgemv(handle,CUBLAS_OP_N, problem_size, problem_size, &alpha, device_matrix,1,device_vector, 1, &beta, device_result,1);
+    cublasStatus_t status_computation = cublasDgemv(handle,CUBLAS_OP_N, problem_size, problem_size, &alpha, device_matrix,problem_size,device_vector, 1, &beta, device_result,1);
     if (status_computation != CUBLAS_STATUS_SUCCESS)
     {
-        cout << "ERROR: cublasDgemv failed. Error code" << status_computation << endl;
+        std::cout << "ERROR: cublasDgemv failed. Error code" << status_computation << std::endl;
     }
 
     cudaEventRecord(stop,0);
     cudaEventSynchronize(stop);
     float time;
     cudaEventElapsedTime(&time, start, stop);
-    cout << time*1e-3 << " s" << endl;
+    std::cout << time*1e-3 << " s" << std::endl;
 
-
+    cudaMemcpy(result, device_result, vec_size, cudaMemcpyDeviceToHost);
+    bool verficiation_passed = verifyResult(result, problem_size, 0.1);
+    if (!verficiation_passed)
+    {
+        std::cerr << "verification failed!" << std::endl;
+    }
     // free memory
     cublasDestroy(handle); 
     
